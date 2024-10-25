b1be476ecb8c62ddfcd73acfabb4c07dc8301162
corirnx <annirocrhor@gmail.com>
2024-10-25 16:19:06 +0200

    setup infrastructure

---
+++ local-consumer/src/app/dashboard/(overview)/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+
+    return (
+        <main className=''>
+            <h1 className={"mb-4 text-xl md:text-2xl text-center"}>
+                Local Consumer
+            </h1>
+            <div className="flex mr-auto ml-auto justify-center">
+                <p>hello!</p>
+            </div>
+        </main>
+    );
+}
